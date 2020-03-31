6feb699141580185b1e1706fee3f96e56c0c6a37
Daniel Rudzinski <danielrudzinski19@gmail.com>
2020-03-31 12:37:46 +0200

    removed submodules

--- src/Startup.cs
+++ src/Startup.cs
@@ -1,15 +1,32 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using Core.Assets;
+using Core.Assets.Models;
+using Core.Database.Payloads;
+using Core.Database.QueryLanguages;
+using Core.Database.Tables;
+using Core.Relationships;
+using Core.Users.Implementation.QueryLanguages;
+using GraphQL;
+using GraphQL.EntityFramework;
+using GraphQL.Types;
+using GraphQL.Utilities;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace Core.Api
 {
@@ -25,7 +42,109 @@ namespace Core.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            Core.Database.BeawreContext.ConnectionString = Configuration.GetConnectionString("DefaultConnection");
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            services.AddMediatR();
+
+            services.AddScoped<Database.IBeawreContext, Database.BeawreContext>();
+            services.AddDbContext<Database.BeawreContext>();
+
+            #region GraphQl
+            GraphTypeTypeRegistry.Register<Asset, AssetGraphQl>();
+            GraphTypeTypeRegistry.Register<AssetModel, AssetGraphQl>();
+            GraphTypeTypeRegistry.Register<Vulnerability, VulnerabilityGraphQl>();
+            GraphTypeTypeRegistry.Register<Risk, RisksGraphQl>();
+            GraphTypeTypeRegistry.Register<Treatment, TreatmentsGraphQl>();
+            //GraphTypeTypeRegistry.Register<Container, ContainerGraphQl>();
+            GraphTypeTypeRegistry.Register<Relationship, RelationshipGraphQl>();
+            GraphTypeTypeRegistry.Register<Evidence, EvidenceGraphQl>();
+            GraphTypeTypeRegistry.Register<RiskPayload, RiskPayloadGraphQl>();
+            GraphTypeTypeRegistry.Register<RiskPayloadModel, RiskPayloadModelGraphQl>();
+            GraphTypeTypeRegistry.Register<TreatmentPayloadModel, TreatmentPayloadModel.TreatmentkPayloadGraphQl>();
+            GraphTypeTypeRegistry.Register<User, UserGraphQl>();
+            //GraphTypeTypeRegistry.Register<Relationship, NotificationGraphQl>();
+            //GraphTypeTypeRegistry.Register<RiskStatus, RiskStatusGraphQl>();
+
+            EfGraphQLConventions.RegisterInContainer(services, new Core.Database.BeawreContext(), userContext => (Core.Database.BeawreContext)userContext);
+
+            services.AddSingleton<AssetGraphQl>();
+            services.AddSingleton<VulnerabilityGraphQl>();
+            services.AddSingleton<RisksGraphQl>();
+            services.AddSingleton<TreatmentsGraphQl>();
+            services.AddSingleton<RelationshipGraphQl>();
+            services.AddSingleton<EvidenceGraphQl>();
+            services.AddSingleton<RiskPayloadGraphQl>();
+            services.AddSingleton<RiskPayloadModelGraphQl>();
+            services.AddSingleton<TreatmentPayloadModel.TreatmentkPayloadGraphQl>();
+            services.AddSingleton<OwaspDictionaryGraphType>();
+            services.AddSingleton<UserGraphQl>();
+
+            foreach (var type in GetGraphQlTypes())
+                services.AddSingleton(type);
+
+            //services.AddGraphQL(options => options.ExposeExceptions = true);
+
+            services.AddSingleton<IDocumentExecuter, EfDocumentExecuter>();
+            services.AddSingleton<IDependencyResolver>(
+                provider => new FuncDependencyResolver(provider.GetRequiredService));
+            //services.AddSingleton<ISchema, Controllers.Schema>();
+            #endregion
+
+            Core.Users.Config.InitializeServices(ref services);
+            Core.Assets.Config.InitializeServices(ref services);
+            Core.Relationships.Config.InitializeServices(ref services);
+            Core.AuditTrail.Config.InitializeServices(ref services);
+            //Core.Evidence.Config.InitializeServices(ref services);
+
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            //services.AddAutoMapper(opt =>
+            //{
+            //    opt.AddProfile(new Core.Users.UsersProfile());
+            //    opt.AddProfile(new AssetsProfile());
+            //    opt.AddProfile(new RelationshipProfile());
+            //    opt.AddProfile(new Core.AuditTrail.CustomProfile());
+            //});
+
+            services.AddControllersWithViews();
+
+            services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                options.ForwardedHeaders =
+                    ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+            });
+
+            services.AddDistributedMemoryCache();
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromHours(24);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
+
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsRules", pol => pol.SetIsOriginAllowed((host) => true).AllowAnyMethod().AllowAnyHeader().AllowCredentials());
+            });
+
+            services.AddMvcCore().AddApiExplorer();
+
+            //services.AddSwaggerGen(c =>
+            //{
+            //    c.SwaggerDoc("v1", new OpenApiInfo() { Title = "Risk Control", Version = "v1" });
+            //});
+
+            // In production, the React files will be served from this directory
+            //services.AddSpaStaticFiles(configuration =>
+            //{
+            //    configuration.RootPath = "ReactApp/build";
+            //});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,5 +166,15 @@ namespace Core.Api
                 endpoints.MapControllers();
             });
         }
+
+        static IEnumerable<Type> GetGraphQlTypes()
+        {
+            return typeof(Startup).Assembly
+                .GetTypes()
+                .Where(x => !x.IsAbstract &&
+                            (typeof(IObjectGraphType).IsAssignableFrom(x) ||
+                             typeof(IInputObjectGraphType).IsAssignableFrom(x)));
+        }
     }
+
 }
